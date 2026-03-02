a3a74e4e74feab939f8485903046ae714e1abfae
Akshat Gupta <akshat4899@gmail.com>
2026-03-03 02:03:58 +0530

    updated

--- HrAssistant/settings.py
+++ HrAssistant/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
+from datetime import timedelta
+
+
 
 # envirnonment variables
 load_dotenv()
@@ -139,3 +142,17 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv("GMAIL_ID")
 EMAIL_HOST_PASSWORD = os.getenv("GMAIL_APP_PASSWORD")
+
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+}
