fa694ae6750e3e5525db7458dead5148f981f435
Akshat Gupta <akshat4899@gmail.com>
2026-03-03 02:59:53 +0530

    updated model

--- Resume/models.py
+++ Resume/models.py
@@ -4,10 +4,12 @@ from candidate.models import Candidate
 
 # Create your models here.
 class Resume(models.Model):
-    candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
+    candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE,related_name="resumes")
     file = models.FileField(upload_to="resumes/")
     parsed_data = models.JSONField(null=True, blank=True)
+    extracted_text = models.TextField(null=True, blank=True)
     resume_score=models.FloatField(null=True, blank=True)
+    is_processed = models.BooleanField(default=False)
     extracted_skills = models.JSONField(null=True, blank=True)
     uploaded_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
