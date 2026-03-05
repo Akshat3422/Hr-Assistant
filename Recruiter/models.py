49109b853a18639d4bfb260200b45626703bea06
Akshat Gupta <akshat4899@gmail.com>
2026-03-05 10:54:46 +0530

    added string name

--- Recruiter/models.py
+++ Recruiter/models.py
@@ -7,3 +7,6 @@ class Recruiter(models.Model):
     company_name = models.CharField(max_length=255)
     phone = models.CharField(max_length=15,blank=True)
     is_verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
